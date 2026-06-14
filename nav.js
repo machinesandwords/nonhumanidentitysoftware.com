755073157e0878fa795e2230f0ea6f7f641caad4
machinesandwords <39410942+machinesandwords@users.noreply.github.com>
2026-06-14 19:55:27 -0400

    Nav

--- nav.js
+++ nav.js
@@ -50,6 +50,7 @@
       <a href="/guides/" class="nav-item depth-0">All guides</a>
       <a href="/guides/nhi-discovery/" class="nav-item depth-1">&#9500; NHI discovery</a>
       <a href="/guides/secrets-vs-workload-identity/" class="nav-item depth-1">&#9500; Secrets vs. workload identity</a>
+      <a href="/guides/rfp-framework/" class="nav-item depth-1">&#9500; RFP Framework</a>
           <a href="/guides/" class="nav-item depth-0">&#9492; more</a>
       </div>
 
